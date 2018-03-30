4d5e6df007894eaac890981a94b636c328a02b5d
rmartintripago <rmartintripago@gmail.com>
2018-03-30 15:44:31 +0530

    updating Code

--- TRIPAGO.sol
+++ TRIPAGO.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.4.20;
+pragma solidity 0.4.21;
 
 /**
  * @title SafeMath
@@ -33,89 +33,180 @@ library SafeMath {
   }
 }
 
-  contract ERC20 {
+contract ERC20 {
   function totalSupply()public view returns (uint total_Supply);
-  function balanceOf(address _owner)public view returns (uint256 balance);
-  function allowance(address _owner, address _spender)public view returns (uint remaining);
-  function transferFrom(address _from, address _to, uint _amount)public returns (bool ok);
-  function approve(address _spender, uint _amount)public returns (bool ok);
-  function transfer(address _to, uint _amount)public returns (bool ok);
-  event Transfer(address indexed _from, address indexed _to, uint _amount);
-  event Approval(address indexed _owner, address indexed _spender, uint _amount);
+  function balanceOf(address who)public view returns (uint256);
+  function allowance(address owner, address spender)public view returns (uint);
+  function transferFrom(address from, address to, uint value)public returns (bool ok);
+  function approve(address spender, uint value)public returns (bool ok);
+  function transfer(address to, uint value)public returns (bool ok);
+  event Transfer(address indexed from, address indexed to, uint value);
+  event Approval(address indexed owner, address indexed spender, uint value);
 }
 
 
 contract TRIPAGO is ERC20
-{using SafeMath for uint256;
-   string public constant symbol = "TPG";
-     string public constant name = "TRIPAGO";
-     uint public constant decimals = 18;
-     uint256 _totalSupply = 1000000000 * 10 ** 18; // 1 Billion Total Supply including 18 decimal
-     
-     // Owner of this contract
-     address public owner;
-     
-  // Balances for each account
-     mapping(address => uint256) balances;
-  
-     // Owner of account approves the transfer of an amount to another account
-     mapping(address => mapping (address => uint256)) allowed;
-  
-     // Functions with this modifier can only be executed by the owner
+{ using SafeMath for uint256;
+    // Name of the token
+    string public constant name = "TRIPAGO";
+
+    // Symbol of token
+    string public constant symbol = "TPG";
+    uint8 public constant decimals = 18;
+    uint public _totalsupply = 1000000000 * 10 ** 18; // 1 Billion inculding decimal precesion
+    address public owner;                    // Owner of this contract
+    uint256 public _price_tokn; 
+    uint256 no_of_tokens;
+    uint256 public pre_startdate;
+    uint256 public ico_startdate;
+    uint256 public pre_enddate;
+    uint256 public ico_enddate;
+    bool stopped = false;
+   
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
+     address ethFundMain = 0x85B442dBD198104F5D43Fbe44F9F8047D9D3705F; 
+
+    
+     enum Stages {
+        NOTSTARTED,
+        PRESALE,
+        ICO,
+        ENDED
+    }
+    Stages public stage;
+    
+    modifier atStage(Stages _stage) {
+        require(stage == _stage);
+        _;
+    }
+    
      modifier onlyOwner() {
-         if (msg.sender != owner) {
-             revert();
-         }
-         _;
-     }
-  
-     // Constructor
-     function TRIPAGO() public {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function TRIPAGO() public
+    {
+        
          owner = msg.sender;
-         balances[owner] = _totalSupply;
-     }
+        balances[owner] = 600000000 * 10 **18;  //600 Million given to Owner
+        balances[address(this)]=  400000000 * 10 **18;  //400 Million given to Smart COntract
+        stage = Stages.NOTSTARTED;
+        emit Transfer(0, owner, balances[owner]);
+        emit  Transfer(0, address(this), balances[address(this)]);
+       
+    }
+    
+     function start_PREICO() public onlyOwner atStage(Stages.NOTSTARTED)
+      {
+          stage = Stages.PRESALE;
+          stopped = false;
+         _price_tokn = 16000;     // 1 Ether = 16000 coins
+          pre_startdate = now;
+          pre_enddate = now + 19 days;
+       
+          }
+    
+    function start_ICO() public onlyOwner atStage(Stages.PRESALE)
+      {
+        //  require(now > pre_enddate);
+          stage = Stages.ICO;
+          stopped = false;
+         _price_tokn = 12000;    // 1 Ether = 12000 coins
+          ico_startdate = now;
+          ico_enddate = now + 31 days;
+     
+      }
   
+  
+    function () public payable 
+    {
+      require(msg.value >= .25 ether);
+        require(!stopped && msg.sender != owner);
+        
+          if( stage == Stages.PRESALE && now <= pre_enddate )
+            { 
+                no_of_tokens =((msg.value).mul(_price_tokn));
+                drain(msg.value);
+                transferTokens(msg.sender,no_of_tokens);
+               }
+               
+                else if(stage == Stages.ICO && now <= ico_enddate )
+            {
+             
+               no_of_tokens =((msg.value).mul(_price_tokn));
+               drain(msg.value);
+               transferTokens(msg.sender,no_of_tokens);
+            }
+        
+        else
+        {
+            revert();
+        }
+       
+    }
+     
+      
+    
+    // called by the owner, pause ICO
+    function StopICO() external onlyOwner 
+    {
+        stopped = true;
+       }
+
+    // called by the owner , resumes ICO
+    function releaseICO() external onlyOwner 
+    {
+        
+        stopped = false;
+      }
+      
+      
+       function end_ICO() external onlyOwner
+     {
+          stage = Stages.ENDED;
+          uint256 x = balances[address(this)];
+         balances[owner] = (balances[owner]).add(balances[address(this)]);
+         balances[address(this)] = 0;
+       emit  Transfer(address(this), owner , x);
+         
+         
+     }
+
+
     // what is the total supply of the ech tokens
      function totalSupply() public view returns (uint256 total_Supply) {
-         total_Supply = _totalSupply;
+         total_Supply = _totalsupply;
      }
-       // What is the balance of a particular account?
+    
+    // What is the balance of a particular account?
      function balanceOf(address _owner)public view returns (uint256 balance) {
          return balances[_owner];
      }
-  
-     // Transfer the balance from owner's account to another account
-     function transfer(address _to, uint256 _amount)public returns (bool ok) {
-        require( _to != 0x0);
-        require(balances[msg.sender] >= _amount && _amount >= 0);
-        balances[msg.sender] = (balances[msg.sender]).sub(_amount);
-        balances[_to] = (balances[_to]).add(_amount);
-        Transfer(msg.sender, _to, _amount);
-             return true;
-         }
-         
+    
     // Send _value amount of tokens from address _from to address _to
      // The transferFrom method is used for a withdraw workflow, allowing contracts to send
      // tokens on your behalf, for example to "deposit" to a contract address and/or to charge
      // fees in sub-currencies; the command should fail unless the _from account has
      // deliberately authorized the sender of the message via some mechanism; we propose
      // these standardized APIs for approval:
-     function transferFrom( address _from, address _to, uint256 _amount )public returns (bool ok) {
+     function transferFrom( address _from, address _to, uint256 _amount )public returns (bool success) {
      require( _to != 0x0);
      require(balances[_from] >= _amount && allowed[_from][msg.sender] >= _amount && _amount >= 0);
      balances[_from] = (balances[_from]).sub(_amount);
      allowed[_from][msg.sender] = (allowed[_from][msg.sender]).sub(_amount);
      balances[_to] = (balances[_to]).add(_amount);
-     Transfer(_from, _to, _amount);
+    emit Transfer(_from, _to, _amount);
      return true;
          }
- 
-     // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
+    
+   // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
      // If this function is called again it overwrites the current allowance with _value.
-     function approve(address _spender, uint256 _amount)public returns (bool ok) {
+     function approve(address _spender, uint256 _amount)public returns (bool success) {
          require( _spender != 0x0);
          allowed[msg.sender][_spender] = _amount;
-         Approval(msg.sender, _spender, _amount);
+       emit  Approval(msg.sender, _spender, _amount);
          return true;
      }
   
@@ -123,6 +214,31 @@ contract TRIPAGO is ERC20
          require( _owner != 0x0 && _spender !=0x0);
          return allowed[_owner][_spender];
    }
-  
 
+     // Transfer the balance from owner's account to another account
+     function transfer(address _to, uint256 _amount)public returns (bool success) {
+        require( _to != 0x0);
+        require(balances[msg.sender] >= _amount && _amount >= 0);
+        balances[msg.sender] = (balances[msg.sender]).sub(_amount);
+        balances[_to] = (balances[_to]).add(_amount);
+       emit Transfer(msg.sender, _to, _amount);
+             return true;
+         }
+    
+          // Transfer the balance from owner's account to another account
+    function transferTokens(address _to, uint256 _amount) private returns(bool success) {
+        require( _to != 0x0);       
+        require(balances[address(this)] >= _amount && _amount > 0);
+        balances[address(this)] = (balances[address(this)]).sub(_amount);
+        balances[_to] = (balances[_to]).add(_amount);
+       emit Transfer(address(this), _to, _amount);
+        return true;
+        }
+    
+    
+    function drain(uint256 value) private {
+         
+        ethFundMain.transfer(value);
+    }
+    
 }
